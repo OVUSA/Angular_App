8a466b2de5eac488ce93cd15d666ce479c9870d4
OVUSA <ovelichko@luc.edu>
2022-06-30 22:02:01 -0500

    ts trying

---
+++ app/src/assets/typeScript.ts
@@ -0,0 +1,17 @@
+
+// define functions
+let doLog = (message: string)=>{
+    console.log(message);
+}
+
+doLog("Hello There");
+
+let drawPoint = (point:{x:number,y:number})=>{
+//..
+}
+
+
+drawPoint({
+    x:1,
+    y:2
+})

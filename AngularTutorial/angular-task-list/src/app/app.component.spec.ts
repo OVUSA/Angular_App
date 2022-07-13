1b0021cadf1c27cddcacdf5d1cc656527f6f760a
OVUSA <ovelichko@luc.edu>
2022-07-12 19:28:10 -0500

    started new project

--- AngularTutorial/angular-task-list/src/app/app.component.spec.ts
+++ AngularTutorial/angular-task-list/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-video-game-db'`, () => {
+  it(`should have as title 'angular-task-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-video-game-db');
+    expect(app.title).toEqual('angular-task-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-video-game-db app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-task-list app is running!');
   });
 });
